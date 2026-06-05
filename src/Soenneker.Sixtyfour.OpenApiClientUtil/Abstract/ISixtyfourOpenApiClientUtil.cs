8385a2f629581a42a22311a0b59c7005901742b7
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:46 -0500

    xml commenting

--- src/Soenneker.Sixtyfour.OpenApiClientUtil/Abstract/ISixtyfourOpenApiClientUtil.cs
+++ src/Soenneker.Sixtyfour.OpenApiClientUtil/Abstract/ISixtyfourOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.Sixtyfour.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface ISixtyfourOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<SixtyfourOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
